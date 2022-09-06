fe9253111e01149947fe49084abf177957c438ff
ahmedgamalelazab <agemy844@gmail.com>
2022-09-06 21:38:52 +0200

    delete Node from the tree has added

--- c#/TreesDataStructure/Tree.cs
+++ c#/TreesDataStructure/Tree.cs
@@ -13,10 +13,53 @@ public class Tree <T> where T : IComparable<T>
 
     public void insert(T element)
     {
-        root = insert(element,root);
+        root = insert(element,root!);
     }
 
+    ///deleting Nodes : 
+    //case 1 : the target Node has no children . [easy to delete]
+    //case 2 : the target Node has one child -> right one or left one 
+    //case 3 : the target node has two children [worst one]
+        //handling the 3rd case : simply replace the value with the smallest right left Node value 
+        //then remove the duplicated tree .
+    public void Remove(T element){
+        root = Remove(element , root!);
+    }   
 
+    private Node? Remove(T element, Node t){
+        if(t == null){
+            return t;
+        }
+        if(element.CompareTo(t.data) < 0){
+            t.left = Remove(element,t.left);
+        }else if(element.CompareTo(t.data) > 0){
+            t.right = Remove(element,t.right);
+        }else if(t.left != null && t.right != null){
+            t.data = FindMin(t.right)!.data;
+            t.right = Remove(t.data , t.right); // remove the item
+        }else {
+            t = (t.left != null) ? t.left : t.right!;
+        }
+        return t;
+    }
+
+    public T? FindMin(){
+        return FindMin(root!)!.data ?? default(T)!;
+    }      
+
+    private Node? FindMin(Node t){
+
+        if(t == null){
+            return null;
+        }
+
+        while(t.left != null){
+            t = t.left;
+        }
+
+        return t;
+
+    }
     private Node insert(T element, Node t)
     {
         if (t == null)
@@ -43,7 +86,7 @@ public class Tree <T> where T : IComparable<T>
 
     public void Print()
     {
-        Print(root);
+        Print(root!);
     }
 
     private void Print(Node t)
